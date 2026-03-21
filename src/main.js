cdcafc22b86695261925e2e3073d745c2cee4a73
trinhhuong <dat.nguyen@codegym.vn>
2026-03-21 20:18:51 +0700

    Tìm kiếm theo tên/tỉnh/thành phố không phân biệt dấu và hoa thường

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import user_data from "./resource/database/client.json" with { type: "json" };
 let userRepo = [...user_data];
 
 function renderUserList(userRepo) {
+    console.log(userRepo);
     let userTableBodyHtml = document.getElementById("userList");
     let content = "";
     for (let i = 0; i < userRepo.length; i++) {
@@ -18,6 +19,50 @@ function renderUserList(userRepo) {
         content += currentContent;
     }
     userTableBodyHtml.innerHTML = content;
+}
+
+function debounce(fn, delay) {
+    let timeout; // biến lưu timer
+
+    return function (...args) {
+        clearTimeout(timeout); // huỷ lần trước
+
+        timeout = setTimeout(() => {
+            fn.apply(this, args); // gọi function gốc
+        }, delay);
+    };
+}
+
+const debounceSearch = debounce((keyword) => {
+    searchUser(keyword);
+}, 500);
+
+function removeVietnameseTones(str) {
+    let rs =  str
+        .normalize("NFD") // tách dấu ra khỏi chữ
+        .replace(/[\u0300-\u036f]/g, "") // xoá dấu
+        .replace(/đ/g, "d")
+        .replace(/Đ/g, "D")
+        .toLowerCase();
+    console.log(rs);
+    return rs;
+}
+
+document.getElementById("searchUser")
+    .addEventListener("input", (e) => {
+        debounceSearch(e.target.value);
+    });
+
+function searchUser(keyword) {
+    let matchingUsers = userRepo.filter(
+        user => {
+            return user.name.includes(keyword) || removeVietnameseTones(user.name).includes(keyword) ||
+                user.province.includes(keyword) || removeVietnameseTones(user.province).includes(keyword) ||
+                    user.city.includes(keyword) || removeVietnameseTones(user.city).includes(keyword)
+        }
+    );
+    console.log(matchingUsers)
+    renderUserList(matchingUsers);
 }
 
 function deleteUser() {
@@ -39,4 +84,5 @@ function addUser() {
 
 window.deleteUser = deleteUser;
 window.addUser = addUser;
+// window.searchUser = searchUser;
 renderUserList(userRepo);
