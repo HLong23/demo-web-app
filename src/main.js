88d6e52742555b2bbf59f78700f79870fe075fd0
HLong23 <longph3655441@gmail.com>
2026-03-24 20:39:37 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ function renderUserList(userRepo) {
             "        <td>" + currentUser.province + "</td>\n" +
             "        <td>" + currentUser.city + "</td>\n" +
             "        <td><button onclick='deleteUser(event.currentTarget.parentNode.parentNode)'>Delete</button></td>\n" +
+            "        <td><button onclick='changeUser(event.currentTarget.parentNode.parentNode)'>Change</button></td>\n" +
             "</tr>";
         content += currentContent;
     }
@@ -67,6 +68,39 @@ function searchUser(keyword) {
     renderUserList(matchingUsers);
 }
 
+function searchByColumn() {
+    let idKeyword = document.getElementById("searchId").value;
+    let nameKeyword = removeVietnameseTones(document.getElementById("searchName").value);
+    let birthKeyword = document.getElementById("searchBirthYear").value;
+    let provinceKeyword = removeVietnameseTones(document.getElementById("searchProvince").value);
+    let cityKeyword = removeVietnameseTones(document.getElementById("searchCity").value);
+
+    let matchingUsers = userRepo.filter(user =>
+        user.id.toString().includes(idKeyword) &&
+        removeVietnameseTones(user.name).includes(nameKeyword) &&
+        user.birthYear.toString().includes(birthKeyword) &&
+        removeVietnameseTones(user.province).includes(provinceKeyword) &&
+        removeVietnameseTones(user.city).includes(cityKeyword)
+    );
+
+    renderUserList(matchingUsers);
+}
+
+document.getElementById("searchId").addEventListener("input", debounceColumnSearch);
+document.getElementById("searchName").addEventListener("input", debounceColumnSearch);
+document.getElementById("searchBirthYear").addEventListener("input", debounceColumnSearch);
+document.getElementById("searchProvince").addEventListener("input", debounceColumnSearch);
+document.getElementById("searchCity").addEventListener("input", debounceColumnSearch);
+
+let timeoutColumn;
+function debounceColumnSearch() {
+    clearTimeout(timeoutColumn);
+
+    timeoutColumn = setTimeout(() => {
+        searchByColumn();
+    }, 300);
+}
+
 function deleteUser(eventTarget) {
     let currentId = eventTarget.firstElementChild.innerHTML;
     let currentIndex = userRepo.findIndex(user => user.id === currentId);
@@ -89,7 +123,45 @@ function addUser() {
     renderUserList(userRepo);
 }
 
+function changeUser(eventTarget) {
+    let button = eventTarget.lastElementChild.lastElementChild;
 
+    if (button.innerHTML === "Change") {
+        let currentName = eventTarget.children[1].innerHTML;
+        let currentBirthYear = eventTarget.children[2].innerHTML;
+        let currentProvince = eventTarget.children[3].innerHTML;
+        let currentCity = eventTarget.children[4].innerHTML;
+
+        eventTarget.children[1].innerHTML = `<input value="${currentName}">`;
+        eventTarget.children[2].innerHTML = `<input value="${currentBirthYear}">`;
+        eventTarget.children[3].innerHTML = `<input value="${currentProvince}">`;
+        eventTarget.children[4].innerHTML = `<input value="${currentCity}">`;
+
+        button.innerHTML = "Apply";
+    } else {
+        let currentId = eventTarget.firstElementChild.innerHTML;
+        let currentIndex = userRepo.findIndex(user => user.id == currentId);
+        let newName = eventTarget.children[1].firstElementChild.value;
+        let newBirthYear = eventTarget.children[2].firstElementChild.value;
+        let newProvince = eventTarget.children[3].firstElementChild.value;
+        let newCity = eventTarget.children[4].firstElementChild.value;
+
+        userRepo[currentIndex].name = newName;
+        userRepo[currentIndex].birthYear = newBirthYear;
+        userRepo[currentIndex].province = newProvince;
+        userRepo[currentIndex].city = newCity;
+
+        eventTarget.children[1].innerHTML = newName;
+        eventTarget.children[2].innerHTML = newBirthYear;
+        eventTarget.children[3].innerHTML = newProvince;
+        eventTarget.children[4].innerHTML = newCity;
+
+        button.innerHTML = "Change";
+    }
+}
+
+window.searchByColumn = searchByColumn;
+window.changeUser = changeUser;
 window.deleteUser = deleteUser;
 window.addUser = addUser;
 window.deleteUser = deleteUser;
